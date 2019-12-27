9ed0d1ba727b5b2ce9e008eeec89c54bfd434bec
Uroš Križman <uros.krizman@gmail.com>
2019-12-27 16:15:34 +0100

    Initial table design

---
+++ src/components/FieldsTable.vue
@@ -0,0 +1,39 @@
+<template>
+  <table class="table table-striped">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">Field</th>
+        <th scope="">Hectares</th>
+        <th scope="col">Crop (season 2020)</th>
+        <th scope="col">Variety</th>
+        <th scope="col">Clover planted</th>
+        <th scope="col">Eco cert.</th>
+        <th scope="col">Acidic soil</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">
+          <strong>BO_6542</strong>
+          <br>
+          <span># 1327296</span>
+        </th>
+        <td>36.08</td>
+        <td>Corn</td>
+        <td>Organic</td>
+        <td>Yes</td>
+        <td>Yes</td>
+        <td>No</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'FieldsTable',
+  props: {
+    msg: String
+  }
+}
+</script>
