7b339aef8a35e233e8f6b5010cf75c653e088a1b
Uroš Križman <uros.krizman@gmail.com>
2019-12-27 18:35:43 +0100

    Dynamic <input> value select example

--- src/components/FieldsTable.vue
+++ src/components/FieldsTable.vue
@@ -1,20 +1,24 @@
 <template>
-  <table class="table table-striped">
-    <thead class="thead-dark">
-      <tr>
-        <th scope="col">Field</th>
-        <th scope="col">Hectares</th>
-        <th scope="col">Crop (season 2020)</th>
-        <th scope="col">Variety</th>
-        <th scope="col">Clover planted</th>
-        <th scope="col">Eco cert.</th>
-        <th scope="col">Acidic soil</th>
-      </tr>
-    </thead>
-    <tbody>
-      <FieldsTableRow :field="field" />
-    </tbody>
-  </table>
+  <div>
+    <table class="table table-striped">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">Field</th>
+          <th scope="col">Hectares</th>
+          <th scope="col">Crop (season 2020)</th>
+          <th scope="col">Variety</th>
+          <th scope="col">Clover planted</th>
+          <th scope="col">Eco cert.</th>
+          <th scope="col">Acidic soil</th>
+        </tr>
+      </thead>
+      <tbody>
+        <FieldsTableRow :field="field" />
+      </tbody>
+    </table>
+    <label for="varietyToggle">External variety change:</label>
+    <input type="number" @change="field.variety = $event.target.value" id="varietyToggle" />
+  </div>
 </template>
 
 <script>
--- src/components/FieldsTableRow.vue
+++ src/components/FieldsTableRow.vue
@@ -7,7 +7,13 @@
     </th>
     <td>{{ field.hectares }}</td>
     <td>{{ field.crop }}</td>
-    <td>{{ field.variety }}</td>
+    <td>
+      <select class="form-control">
+        <option value="1" :selected="field.variety == 1">One</option>
+        <option value="2" :selected="field.variety == 2">Two</option>
+        <option value="3" :selected="field.variety == 3">Three</option>
+      </select>
+    </td>
     <td>{{ field.cloverPlanted }}</td>
     <td>{{ field.ecoCert }}</td>
     <td>{{ field.acidicSoil}}</td>
